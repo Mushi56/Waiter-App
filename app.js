f85ca8701406b96f8e4804fb6f2b268271fc00ff
Mushtaq Ahmad <mushtaq@example.com>
2026-04-25 16:52:03 +0800

    Reverted modal close icons to cross and changed menu card delete icons to trash cans

--- app.js
+++ app.js
@@ -519,7 +519,9 @@
           <button class="manage-card-edit" data-edit-id="${item.id}" title="Edit">
             <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
           </button>
-          <button class="manage-card-delete" data-del-id="${item.id}" title="Delete">✕</button>
+          <button class="manage-card-delete" data-del-id="${item.id}" title="Delete">
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M3 6h18m-2 0v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"/></svg>
+          </button>
         </div>
         ${item.image
           ? `<img class="manage-card-img" src="${item.image}" alt="${item.name}" loading="lazy">`
